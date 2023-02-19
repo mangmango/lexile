929dd577ac6f806635501c4319ec0caedfe4db77
mangmango <permansu723@gmail.com>
2023-02-19 20:16:27 +0900

    init

---
+++ app/src/main/java/com/example/myapplication/NavItem.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+class NavItem(val name: String, val route: String, val icon: ImageVector, val index: Int)
